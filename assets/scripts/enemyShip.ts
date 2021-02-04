77e8e2a6fd24719d8a6a2ea6e1d8eb9f67285f26
hsimao <e087754958@gmail.com>
2021-02-04 19:37:52 +0800

    add: 新增敵機移動到範圍外 重新回到Menu 場景邏輯

--- assets/scripts/enemyShip.ts
+++ assets/scripts/enemyShip.ts
@@ -41,21 +41,39 @@ export default class NewClass extends cc.Component {
     return cc.repeatForever(cc.sequence(moveLeft, moveRight));
   }
 
-  // LIFE-CYCLE CALLBACKS:
-
-  onLoad() {
+  init() {
+    // 執行移動
     this.moveEnemy = this.setMovements();
     this.node.runAction(this.moveEnemy);
-    // 依據 shootFrequency 頻率, 發射子彈
+
+    // 依據 shootFrequency 頻率, 持續發射子彈
     this.schedule(
       this.shootBullets,
       this.shootFrequency,
       cc.macro.REPEAT_FOREVER,
       0
     );
+
+    cc.director.preloadScene("Menu");
+  }
+
+  handleGameEnd() {
+    // 如果敵機移動到銀幕外面, 銷毀敵機, 轉到 Menu 場景
+    if (
+      Math.abs(this.node.position.y) >= this.node.parent.getContentSize().height
+    ) {
+      this.node.destroy();
+      cc.director.loadScene("Menu");
+    }
+  }
+
+  onLoad() {
+    this.init();
   }
 
   start() {}
 
-  // update (dt) {}
+  update(dt) {
+    this.handleGameEnd();
+  }
 }
