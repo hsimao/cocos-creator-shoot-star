d3f9e9f6e7be84a0dce85b8585cedf35ab1f2a63
hsimao <e087754958@gmail.com>
2021-02-04 13:39:02 +0800

    add: 新增 start 場景, 以及切換場景邏輯

---
+++ assets/scripts/switchScene.ts
@@ -0,0 +1,17 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass } = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+  onLoad() {
+    cc.director.preloadScene("Game");
+
+    this.node.on("touchstart", () => cc.director.loadScene("Game"), this);
+  }
+}
